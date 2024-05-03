0fd08559196297c995ae31267028694bdc7c6e9b
Approximations <datkidbosst@gmail.com>
2024-05-03 19:59:01 -0300

    refactor: change JwtTokenFilter to use cookies to grab token

--- client-server/src/main/java/me/approximations/security/jwt/filter/JwtTokenFilter.java
+++ client-server/src/main/java/me/approximations/security/jwt/filter/JwtTokenFilter.java
@@ -4,6 +4,7 @@ import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -17,33 +18,27 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
 
 import java.io.IOException;
 
 @RequiredArgsConstructor
 @Service
 public class JwtTokenFilter extends OncePerRequestFilter {
-    private static final String BEARER_PREFIX = "Bearer ";
-
     private final JwtTokenService jwtTokenService;
     private final CustomUserDetailsService userDetailsService;
     private final CustomAuthenticationEntryPoint authenticationEntryPoint;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+        final Cookie cookie = WebUtils.getCookie(request, "token");
 
-        if (header == null || !header.startsWith(BEARER_PREFIX)) {
+        if (cookie == null || cookie.getValue().isEmpty()) {
             filterChain.doFilter(request, response);
             return;
         }
 
-        final String token = header.replace(BEARER_PREFIX, "");
-
-        if (token.isEmpty()) {
-            authenticationEntryPoint.commence(request, response);
-            return;
-        }
+        final String token = cookie.getValue();
 
         try {
             final DecodedJWT jwt = jwtTokenService.decodeToken(token);
