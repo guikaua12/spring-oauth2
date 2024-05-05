8f0f3de9db0e27c820fe66a489e26c3a2372f999
Approximations <datkidbosst@gmail.com>
2024-05-05 15:53:49 -0300

    feat: added warning messages on login page

--- front-end/src/app/(auth)/auth/login/page.tsx
+++ front-end/src/app/(auth)/auth/login/page.tsx
@@ -1,11 +1,41 @@
+"use client";
 import React from "react";
 import AuthProviderButton from "@/components/AuthProviderButton/auth-provider-button";
 import LoginForm from "@/feature/auth/LoginForm/login-form";
+import { useAuth } from "@/feature/auth/hooks/useAuth";
+import Warning from "@/components/Warning/warning";
+
+type Status = "error" | "success" | "idle" | "pending";
+
+const getWarningType = (status: Status): "success" | "error" => {
+    if (status === "success") return "success";
+    if (status === "error") return "error";
+    return "success";
+};
+
+const warningOpened = (status: Status) => {
+    return status === "success" || status === "error";
+};
 
 const LoginPage = () => {
+    const { login } = useAuth();
+
+    const warningContent = (): string => {
+        if (login.isSuccess) return "Logged in successfully";
+        if (login.error && login.error.response) {
+            return login.error.response.data.message ?? login.error.response.data.error;
+        }
+
+        return "Unknown error";
+    };
+
     return (
         <div className="flex h-screen items-center justify-center">
             <main className="w-96">
+                <Warning open={warningOpened(login.status)} className="mb-2" type={getWarningType(login.status)}>
+                    {warningContent()}
+                </Warning>
+
                 <h1 className="text-center text-3xl font-semibold">Welcome Back</h1>
                 <p className="text-center font-medium">Login into your account</p>
 
